57b8ca9aa87540e78c96f397f9dd5c0bb4b86008
Tenthen <tenthentrino@gmail.com>
2025-10-03 23:27:27 -0600

    Looping to draw a triangle pattern.

---
+++ js/basics/looping-triangle.js
@@ -0,0 +1,11 @@
+
+
+let pattern = "#";  //Binding for holding the hashtag state.
+
+//for iterating the printing out of the pattern.
+for (let i = 0; i < 7; i++) {
+
+  console.log(pattern); //Prints out the pattern.
+  pattern += '#';       //Updates the number of hashtags in the pattern.
+
+}
