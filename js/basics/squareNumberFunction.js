4eabc5218aece782138c1e59b3941ca054b43001
Tenthen <tenthentrino@gmail.com>
2025-10-04 02:32:41 -0600

    Basic function that returns square of numbers.

---
+++ js/basics/squareNumberFunction.js
@@ -0,0 +1,9 @@
+
+// Returns the square of the given number
+
+const square = function(number){
+    return number * number;
+};
+
+let num = 8;
+console.log(`The square of the number ${num} is: ${square(num)}`);
