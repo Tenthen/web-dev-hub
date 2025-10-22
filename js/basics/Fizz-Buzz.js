0b10876d7ecdd25a7ef266d72220fde8acecf79a
Tenthen <tenthentrino@gmail.com>
2025-10-22 06:06:16 -0600

    Add some modification to display the word FizzBuzz too.

--- js/basics/Fizz-Buzz.js
+++ js/basics/Fizz-Buzz.js
@@ -1,12 +1,18 @@
-
-for (let i = 1; i <= 100; i++) {
-    if(i % 3 == 0) {
-        console.log('Fizz');
-        continue;
-    } else if (i % 5 == 0) {
-        console.log('Buzz');
-        continue;
-    } else {
-        console.log(i);
-    }
+function fizz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+  }
 }
+
+function fizzBuzz() {
+  for (let counter = 1; counter <= 100; counter++) {
+    if (counter % 3 == 0 && counter % 5 == 0) console.log("FizzBuzz");
+    else if (counter % 3 == 0) console.log("Fizz");
+    else if (counter % 5 == 0) console.log("Buzz");
+    else console.log(counter);
+  }
+}
+
+fizzBuzz();
