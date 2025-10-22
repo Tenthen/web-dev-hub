a69d4e622ab6c13b87f77af1b6c198dc79a57eaa
Tenthen <tenthentrino@gmail.com>
2025-10-21 21:43:24 -0600

    Some modifications on the computation of special number.

--- js/basics/specialNumbers.js
+++ js/basics/specialNumbers.js
@@ -6,13 +6,17 @@ function specialNumbers (num1, num2) {
     let result2 = num2/num2;
     let result3 = num1 - num1;
     let result4 = 2/num1;
-    let result5 = 2/num2;
+    let result5 = 2000000000000000/num2;
 
     console.log(`Ifinity - Infinity is ${result3}`);
     console.log(`Zero devided by zero is: ${result2}`);
     console.log(`Substracting any whole number from Infinity is: ${result1}`);
     console.log(`Any whole number divided by Infinity is: ${result4}`);
     console.log(`Any whole number divided by zero is: ${result5}`);
+    console.log(`Infinity devide by Infinty is ${num1/num1}`);
+    console.log(`Infinity times Infinity ${num1 * num1}`);
+    console.log(`NaN devide by NaN is ${(result3 * result3)}`);
+    console.log(`Zero times Ifinity is: ${num1 * num2}`);
 
 }
 
