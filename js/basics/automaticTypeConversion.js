e2d26ea135879a3456176b7b310388c8864c1310
Tenthen <tenthentrino@gmail.com>
2025-10-27 22:31:38 -0600

    Getting familiar with the type conversion.

---
+++ js/basics/automaticTypeConversion.js
@@ -0,0 +1,15 @@
+
+console.log('five' * 2);
+console.log('5' * 2);
+
+console.log(NaN == NaN);
+console.log(null == null);
+console.log(undefined == undefined);
+console.log(null == undefined);
+console.log(0 == null);
+console.log(0 == undefined);
+console.log(0 == false)
+console.log('Mulie' * 'Tenthen');
+console.log('2' + 1);
+console.log(2 + '1');
+console.log(1 || 2)
